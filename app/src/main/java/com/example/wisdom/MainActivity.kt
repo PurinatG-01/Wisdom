ac7584a2ceab699e0e927456c689e03a74bf4f37
Teerakarn <sl16394@hotmail.com>
2020-04-30 22:34:08 +0700

    added navbar

--- app/src/main/java/com/example/wisdom/MainActivity.kt
+++ app/src/main/java/com/example/wisdom/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.wisdom
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -9,6 +10,13 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -23,7 +31,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var database: DatabaseReference
 
     private lateinit var userEmail: String
-
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,31 +44,69 @@ class MainActivity : AppCompatActivity() {
         userEmail = intent.getStringExtra("email")
         setUI()
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav)
+        toolbar = findViewById(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+        val toggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        ) {
+            override fun onDrawerClosed(view: View) {
+                super.onDrawerClosed(view)
+                //toast("Drawer closed")
+            }
+
+            override fun onDrawerOpened(drawerView: View) {
+                super.onDrawerOpened(drawerView)
+                //toast("Drawer opened")
+            }
+        }
+
+//        toggle.isDrawerIndicatorEnabled = true
+        navView.bringToFront()
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        toggle.isDrawerIndicatorEnabled = true
+        supportActionBar?.setHomeButtonEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
 
 
     }
 
 
+    private fun setUI() {
 
-
-    private fun setUI(){
-
-        database.addValueEventListener(object: ValueEventListener{
+        database.addValueEventListener(object : ValueEventListener {
             override fun onCancelled(p0: DatabaseError) {}
             override fun onDataChange(p0: DataSnapshot) {
-                if(p0.exists()){
+                if (p0.exists()) {
 
-                    card1Name.text = p0.child("event").child("0").child("event_name").getValue().toString()
-                    card1Donation.text = p0.child("event").child("0").child("total_donation").getValue().toString()
-                    EventActivity.DownLoadImageTask(eventCard1).execute(p0.child("event").child("0").child("image_url").getValue().toString())
+                    card1Name.text =
+                        p0.child("event").child("0").child("event_name").getValue().toString()
+                    card1Donation.text =
+                        p0.child("event").child("0").child("total_donation").getValue().toString()
+                    EventActivity.DownLoadImageTask(eventCard1)
+                        .execute(p0.child("event").child("0").child("image_url").getValue().toString())
 
-                    card2Name.text = p0.child("event").child("1").child("event_name").getValue().toString()
-                    card2Donation.text = p0.child("event").child("1").child("total_donation").getValue().toString()
-                    EventActivity.DownLoadImageTask(eventCard2).execute(p0.child("event").child("1").child("image_url").getValue().toString())
+                    card2Name.text =
+                        p0.child("event").child("1").child("event_name").getValue().toString()
+                    card2Donation.text =
+                        p0.child("event").child("1").child("total_donation").getValue().toString()
+                    EventActivity.DownLoadImageTask(eventCard2)
+                        .execute(p0.child("event").child("1").child("image_url").getValue().toString())
 
-                    card3Name.text = p0.child("event").child("2").child("event_name").getValue().toString()
-                    card3Donation.text = p0.child("event").child("2").child("total_donation").getValue().toString()
-                    EventActivity.DownLoadImageTask(eventCard3).execute(p0.child("event").child("2").child("image_url").getValue().toString())
+                    card3Name.text =
+                        p0.child("event").child("2").child("event_name").getValue().toString()
+                    card3Donation.text =
+                        p0.child("event").child("2").child("total_donation").getValue().toString()
+                    EventActivity.DownLoadImageTask(eventCard3)
+                        .execute(p0.child("event").child("2").child("image_url").getValue().toString())
 
 
                 }
@@ -69,36 +117,50 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun goEvent(view : View){
+    fun goEvent(view: View) {
 
         val intent = Intent(this, EventActivity::class.java)
         val id = view.id
 
         var eventId = "0";
-        when(view.id) {
-            R.id.eventCard1 -> {eventId = "0"}
-            R.id.eventCard2 -> {eventId = "1"}
-            R.id.eventCard3 -> {eventId = "2"}
+        when (view.id) {
+            R.id.eventCard1 -> {
+                eventId = "0"
+            }
+            R.id.eventCard2 -> {
+                eventId = "1"
+            }
+            R.id.eventCard3 -> {
+                eventId = "2"
+            }
         }
-        intent.putExtra("id",eventId)
+        intent.putExtra("id", eventId)
         startActivity(intent)
 
 
     }
 
-    fun goCategory(view : View){
+    fun goCategory(view: View) {
 
         val intent = Intent(this, CategoryActivity::class.java)
         val id = view.id
 
         var categoryName = "";
-        when(view.id) {
-            R.id.buttonC1 -> {categoryName = "Technology"}
-            R.id.buttonC2 -> {categoryName = "Environment"}
-            R.id.buttonC3 -> {categoryName = "Festival"}
-            R.id.buttonC4 -> {categoryName = "Sport"}
+        when (view.id) {
+            R.id.buttonC1 -> {
+                categoryName = "Technology"
+            }
+            R.id.buttonC2 -> {
+                categoryName = "Environment"
+            }
+            R.id.buttonC3 -> {
+                categoryName = "Festival"
+            }
+            R.id.buttonC4 -> {
+                categoryName = "Sport"
+            }
         }
-        intent.putExtra("categoryName",categoryName)
+        intent.putExtra("categoryName", categoryName)
         startActivity(intent)
 
 
