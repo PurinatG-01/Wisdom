05dc79c8178a92318374387d089d630615dac80b
swindlemek <swindle.mek@hotmail.com>
2020-05-02 01:39:02 +0700

    Add name and surname in SignUp

--- app/src/main/java/com/example/wisdom/SignupActivity.kt
+++ app/src/main/java/com/example/wisdom/SignupActivity.kt
@@ -7,28 +7,54 @@ import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_signup.*
 
 class SignupActivity : AppCompatActivity() {
 
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var database: DatabaseReference
+
+    @IgnoreExtraProperties
+    data class User(
+                    var email: String? = "",
+                    var list_event: MutableList<String> = mutableListOf(),
+                    var name: String? = "",
+                    var surname: String? = "",
+                    var total_donation: Int = 0
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
-
+        database = Firebase.database.reference
         auth = FirebaseAuth.getInstance()
 
         signupButton.setOnClickListener(){
             signUpUser()
         }
 
+
     }
 
     fun signUpUser(){
         if(emailText.text.toString().isEmpty()){
             emailText.error = "Please enter email"
+            emailText.requestFocus()
+            return
+        }
+
+        if(nameSignup.text.toString().isEmpty()){
+            emailText.error = "Please enter name"
+            emailText.requestFocus()
+            return
+        }
+
+        if(surnameSignup.text.toString().isEmpty()){
+            emailText.error = "Please enter surname"
             emailText.requestFocus()
             return
         }
@@ -59,9 +85,11 @@ class SignupActivity : AppCompatActivity() {
 
         auth.createUserWithEmailAndPassword(emailText.text.toString(),passwordText.text.toString()).addOnCompleteListener(this){task ->
             if(task.isSuccessful){
+                var firebaseDatabase = FirebaseDatabase.getInstance()
+                var user = User( email = emailText.text.toString(), name = nameSignup.text.toString(), surname = surnameSignup.text.toString() )
+                database.child("user").push().setValue(user)
                 startActivity(Intent(this,MainActivity::class.java))
                 finish()
-
             }
             else{
                 Toast.makeText(baseContext, "Sign up  failed.", Toast.LENGTH_SHORT).show()
